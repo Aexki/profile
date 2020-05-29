954dcf2065803e51bef6add7070f5c10981f1dc5
Aexki <50047387+Aexki@users.noreply.github.com>
2020-05-29 12:52:12 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function go() {
     document.getElementById("aboutme").style.display = "block"
     document.getElementById("skill").style.display = "block"
-    document.documentElement.scrollTop = screen.height
+    document.documentElement.scrollTop = screen.height - 70
 }
