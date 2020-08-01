e45b984eb6d1bed2a4588045d465a9f2be978915
Aman Jena <50047387+Aexki@users.noreply.github.com>
2020-08-01 18:33:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,5 +61,5 @@ function contact() {
 $.getJSON("https://api.ipify.org?format=json",
     function(json) {
 //         $.post("https://script.google.com/macros/s/AKfycbzkgyddBmkLoR-uN4Eufy6hR1Lta8DVpnXCn9rXGGcJvHvUZmU/exec", json)
-           console.log(json)
+           console.log(json.ip)
     });
